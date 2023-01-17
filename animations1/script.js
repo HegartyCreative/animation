df6adc1c0ff3b45f36a5e5c240575999a1b30e92
Sean Hegarty <sean.hegarty@btinternet.com>
2023-01-17 09:18:52 +0000

    My first commit

---
+++ animations1/script.js
@@ -0,0 +1,99 @@
+let select = (e) => document.querySelector(e);
+let selectAll = (e) => document.querySelectoAll(e);
+
+let pause = select("#pause");
+
+let animation = gsap.timeline({
+  onComplete: () => (pause.innerHTML = "Play"),
+});
+animation.pause();
+
+animation
+
+  .fromTo(
+    ".logo",
+    { opacity: 1 },
+    { opacity: 0, duration: 1, ease: "power2.out" },
+    "+=1"
+  )
+  .fromTo(
+    ".model",
+    { x: 80, opacity: 0, scale: 0.6 },
+    { x: 0, opacity: 1, duration: 1.5, scale: 1, ease: "power2.out" }
+  )
+  .fromTo(
+    ".one",
+    { x: 35, opacity: 0 },
+    { x: 0, opacity: 1, duration: 2, ease: "power4.out" },
+    "-=1"
+  )
+  .fromTo(
+    ".four",
+    { x: -29, opacity: 0 },
+    { x: 0, opacity: 1, duration: 2, ease: "power4.out" },
+    "<"
+  )
+  .fromTo(
+    ".two",
+    { y: -29, opacity: 0 },
+    { y: 0, opacity: 1, duration: 2, ease: "power4.out" },
+    "<"
+  )
+  .fromTo(
+    ".three",
+    { y: 29, opacity: 0 },
+    { y: 0, opacity: 1, duration: 2, ease: "power4.out" },
+    "<"
+  )
+
+  .fromTo(
+    ".everywhere",
+    { y: -15, opacity: 0 },
+    { y: 0, opacity: 1, duration: 1, ease: "power4.out" },
+    "-=1.5"
+  )
+
+  .fromTo(
+    ".brand",
+    { y: -15, opacity: 0 },
+    { y: 0, opacity: 1, duration: 1, ease: "power4.out" },
+    "-=0.75"
+  )
+
+  .fromTo(
+    ".show",
+    { opacity: 0 },
+    { opacity: 1, duration: 0.5, ease: "power4.out" },
+    "-=0.5"
+  )
+
+  .to(
+    ".brand",
+    {
+      opacity: 0,
+    },
+    "+=2.0"
+  )
+
+  .to(
+    ".show",
+    {
+      y: -40,
+      duration: 0.5,
+    },
+    "<"
+  )
+
+  .fromTo(
+    ".logo2",
+    { opacity: 0 },
+    { opacity: 1, duration: 1, ease: "power4.out" }
+  );
+
+pause.addEventListener("click", () => {
+  animation.paused(!animation.paused());
+  if (animation.progress() == 1) {
+    animation.restart();
+  }
+  pause.innerHTML = animation.paused() ? "Play" : "Pause";
+});
