97e205eab86844bc5235b872e87cbf33152c8ecd
Sean Hegarty <sean.hegarty@btinternet.com>
2023-02-10 17:21:22 +0000

    Added animation on City Breaks

--- animations4/script.js
+++ animations4/script.js
@@ -3,66 +3,234 @@ let selectAll = (e) => document.querySelectoAll(e);
 
 let pause = select("#pause");
 
-let travel = gsap.timeline({
+let master = gsap.timeline({
   onComplete: () => (pause.innerHTML = "Play"),
 });
-travel.pause();
-travel.to(".child1", {
-  scale: 1.1,
-  duration: 7,
-  transformOrigin: "50% 100%",
-  ease: "none",
+
+master.pause();
+
+master.to(".start", {
+  opacity: 0,
+  delay: 2,
 });
-travel.fromTo(".quote1", { opacity: 0 }, { opacity: 1 }, "=-6");
-travel.fromTo(".source1", { opacity: 0 }, { opacity: 1 }, "=-5");
-travel.to("child1, .quote1, .source1", { opacity: 0, duration: 0 });
 
-travel.fromTo(
-  ".child2",
+// Barcelona
+
+master.to(".barcelona", {
+  scale: 1.1,
+  ease: "none",
+  transformOrigin: "center center",
+  duration: 5,
+});
+
+master.to(
+  ".start_text",
   {
     opacity: 0,
   },
+  "=-6"
+);
+
+master.to(
+  ".barcelona_text1, .barcelona_text2",
   {
     opacity: 1,
-    duration: 0.25,
-  }
+  },
+  "<"
 );
-travel.fromTo(
-  ".child2",
-  { scale: 1 },
-  { scale: 1.1, duration: 8, transformOrigin: "50% 100%", ease: "none" },
-  "=-1"
-);
-travel.fromTo(".quote2", { opacity: 0 }, { opacity: 1 }, "=-6");
-travel.fromTo(".source2", { opacity: 0 }, { opacity: 1 }, "=-5");
-travel.to("child2, .quote2, .source2", { opacity: 0, duration: 0 });
 
-travel.fromTo(
-  ".child3",
+master.to(
+  ".barcelona, .barcelona_text1, .barcelona_text2",
   {
-    opacity: 0,
+    x: 320,
+    ease: "none",
+    duration: 0.25,
+  },
+  "=+0.25"
+);
+
+master.fromTo(
+  ".box1",
+  {
+    x: -300,
   },
   {
-    opacity: 1,
+    x: 300,
+    ease: "none",
     duration: 0.25,
-  }
+  },
+  "=-0.25"
 );
-travel.fromTo(
-  ".child3",
-  { scale: 1 },
-  { scale: 1.1, duration: 8, transformOrigin: "50% 100%", ease: "none" },
-  "=-1"
-);
-travel.fromTo(".quote3", { opacity: 0 }, { opacity: 1 }, "=-6");
-travel.fromTo(".source3", { opacity: 0 }, { opacity: 1 }, "=-5");
-travel.to(".parent1, .parent2, .parent3", { opacity: 0 });
 
-travel.to(".end", { opacity: 1 });
+master.to(
+  ".venice, .venice_text1, .venice_text2",
+  {
+    scale: 1.05,
+    ease: "none",
+    transformOrigin: "center center",
+    duration: 5,
+    stagger: 0.5,
+  },
+  "=-0.5"
+);
+
+master.to(
+  ".venice_text1, .venice_text2",
+  {
+    opacity: 1,
+  },
+  "=-5"
+);
+
+// Rome
+
+master.to(
+  ".venice, .venice_text1, .venice_text2",
+  {
+    x: 320,
+    ease: "none",
+    duration: 0.25,
+  },
+  "=+0.25"
+);
+
+master.fromTo(
+  ".box1",
+  {
+    x: -300,
+  },
+  {
+    x: 300,
+    ease: "none",
+    duration: 0.25,
+  },
+  "=-0.25"
+);
+
+master.to(
+  ".rome, .rome_text1, .rome_text2",
+  {
+    scale: 1.05,
+    ease: "none",
+    transformOrigin: "center center",
+    duration: 5,
+    stagger: 0.5,
+  },
+  "=-0.5"
+);
+
+master.to(
+  ".rome_text1, .rome_text2",
+  {
+    opacity: 1,
+  },
+  "=-5"
+);
+
+// Monte Carlo
+
+master.to(
+  ".rome, .rome_text1, .rome_text2",
+  {
+    x: 320,
+    ease: "none",
+    duration: 0.25,
+  },
+  "=+0.25"
+);
+
+master.fromTo(
+  ".box1",
+  {
+    x: -300,
+  },
+  {
+    x: 300,
+    ease: "none",
+    duration: 0.25,
+  },
+  "=-0.25"
+);
+
+master.to(
+  ".berlin, .berlin_text1, .berlin_text2",
+  {
+    scale: 1.05,
+    ease: "none",
+    transformOrigin: "center center",
+    duration: 5,
+    stagger: 0.5,
+  },
+  "=-0.5"
+);
+
+master.to(
+  ".berlin_text1, .berlin_text2",
+  {
+    opacity: 1,
+  },
+  "=-5"
+);
+
+// Amsterdam
+
+master.to(
+  ".berlin, .berlin_text1, .berlin_text2",
+  {
+    x: 320,
+    ease: "none",
+    duration: 0.25,
+  },
+  "=+0.25"
+);
+
+master.fromTo(
+  ".box1",
+  {
+    x: -300,
+  },
+  {
+    x: 300,
+    ease: "none",
+    duration: 0.25,
+  },
+  "=-0.25"
+);
+
+master.to(
+  ".montpelier, .montpelier_text1, .montpelier_text2",
+  {
+    scale: 1.05,
+    ease: "none",
+    transformOrigin: "center center",
+    duration: 5,
+    stagger: 0.5,
+  },
+  "=-0.5"
+);
+
+master.to(
+  ".montpelier_text1, .montpelier_text2",
+  {
+    opacity: 1,
+  },
+  "=-5"
+);
+
+master.to(".end", {
+  opacity: 1,
+});
+
+master.to(".end_text", {
+  opacity: 1,
+});
+
+// Button code
 
 pause.addEventListener("click", () => {
-  travel.paused(!travel.paused());
-  if (travel.progress() == 1) {
-    travel.restart();
+  master.paused(!master.paused());
+  if (master.progress() == 1) {
+    master.restart();
   }
-  pause.innerHTML = travel.paused() ? "Play" : "Pause";
+  pause.innerHTML = master.paused() ? "Play" : "Pause";
 });
